55f101247d93e13d3ebf45a25a7cbd8dbd64dfb6
Artur Dembicki <artur.dembicki@gmail.com>
2019-03-26 11:34:00 +0100

    add pow, roots and modulus paragraphs

---
+++ arithmetics.py
@@ -0,0 +1,26 @@
+# arithmetic in Python:
+
+a = 13
+b = 2
+
+# power:
+c = a**b
+
+# square root:
+d = 2**.5
+from math import sqrt
+e = sqrt(a)
+
+# n-root:
+f = a**(1/b)
+
+# modulus (remainder):
+g = a % b
+
+print(c, "\n",
+      d, "\n",
+      e, "\n",
+      f, "\n",
+      round(f), "\n",
+      g, "\n"
+      )
