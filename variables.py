689dad2ced5f9c86dbe3c0f7d2ce0c5a2c1c3375
Artur Dembicki <artur.dembicki@gmail.com>
2019-03-29 09:23:30 +0100

    small changes

--- variables.py
+++ variables.py
@@ -1,11 +1,16 @@
-#deklarowanie zmiennych
-#python sam definiuje typy
+# deklarowanie zmiennych
+# python sam definiuje typy
+'''
+many lines comment
+many lines comment
+many lines comment
+'''
 
 print("variables in python:")
 
-zmienna1 = "somecharacters"     #<class 'str'>
-zmienna2 = 123                  #<class 'int'>
-zmienna3 = -0.1                 #<class 'float'>
+zmienna1 = "somecharacters"     # <class 'str'>
+zmienna2 = 123                  # <class 'int'>
+zmienna3 = -0.1                 # <class 'float'>
 
 print(zmienna1, type(zmienna1))
 print("    ", zmienna1 + "abc", " --> dodawanie stringów")
@@ -15,31 +20,31 @@ print(zmienna3, type(zmienna3))
 print()
 
 
-#właściwości typu <class 'bool'>
+# właściwości typu <class 'bool'>
 print("bool type properties:")
 
-zmienna4 = False    #false  <class 'bool'>
-zmienna5 = -False   #0      <class 'int'>
+zmienna4 = False    # false  <class 'bool'>
+zmienna5 = -False   # 0      <class 'int'>
 print(zmienna4, type(zmienna4))
 print(zmienna5, type(zmienna5))
 print()
 
-zmienna4 = True     #true   <class 'bool'>
-zmienna5 = -True    #-1     <class 'int'>
+zmienna4 = True     # true   <class 'bool'>
+zmienna5 = -True    # -1     <class 'int'>
 print(zmienna4, type(zmienna4))
 print(zmienna5, type(zmienna5))
 print()
 
 zmienna4 = 15
 bool(zmienna4)
-print(zmienna4, type(zmienna4)) #dlaczego typem jest teraz <class 'int'> ?
+print(zmienna4, type(zmienna4)) # dlaczego typem jest teraz <class 'int'> ?
 zmienna5 = 0
 bool(zmienna5)
-print(zmienna5, type(zmienna5)) #dlaczego typem jest teraz <class 'int'> ?
+print(zmienna5, type(zmienna5)) # dlaczego typem jest teraz <class 'int'> ?
 print()
 
 
-#getting user input
+# getting user input
 print("user input:")
 
 print("type in a variable:")
@@ -51,5 +56,5 @@ print()
 print("user numbers input:")
 print("type in a number:")
 x = input()
-x = int(x)      #type conversion string --> int
+x = int(x)      # type conversion string --> int
 print("variable", x, "has now been converted to", type(x))
